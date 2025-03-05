805ad870d4087d92ec0f148ea0cd3f8f732f58ad
ranggaptr <rangga2000putera@gmail.com>
2025-03-05 10:44:55 +0700

    PWL-4-PRAKTIKUM-2.1-2.3

--- PERTEMUAN 2/JOBSHEET/app/Http/Controllers/UserController.php
+++ PERTEMUAN 2/JOBSHEET/app/Http/Controllers/UserController.php
@@ -25,16 +25,41 @@ class UserController extends Controller
         // $user =UserModel::all();
         // return view('user',['data'=>$user]);
 
-        $data=[
-            'level_id'=>2,
-            'username'=>'manager-3',
-            'nama'=>'Manager_3',
-            'password'=>Hash::make('12345'),
-        ];
-        UserModel::create($data);
+        // $data=[
+        //     'level_id'=>2,
+        //     'username'=>'manager-3',
+        //     'nama'=>'Manager_3',
+        //     'password'=>Hash::make('12345'),
+        // ];
+        // UserModel::create($data);
         
-        // akses table
-        $user =UserModel::all();
+        // // akses table
+        // $user =UserModel::all();
+        // return view('user',['data'=>$user]);
+
+        // $user=UserModel::find(1);
+        // return view('user',['data'=>$user]);
+
+        // $user=UserModel::Where('level_id',1)->first();
+        // return view('user',['data'=>$user]);
+
+        // $user=UserModel::firstWhere('level_id',1);
+        // return view('user',['data'=>$user]);
+
+        // $user=UserModel::findOr(20,['username','nama'],function(){
+        //     abort(404);
+        // });
+        // return view('user',['data'=>$user]);
+
+        // $user=UserModel::findOrFail(100);
+        // return view('user',['data'=>$user]);
+        
+        // $user=UserModel::where('username','manager-3')->firstOrFail();
+        // return view('user',['data'=>$user]);
+
+
+        $user=UserModel::where('level_id',2)->count();
+        // dd($user);
         return view('user',['data'=>$user]);
     }
 }
